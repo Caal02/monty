87329b6ae6168fcf68b36f7731ef6e9f3821aca6
Caleb <caaleb02@gmail.com>
2022-10-07 09:23:01 -0700

    correct all

--- monty.h
+++ monty.h
@@ -1,40 +1,11 @@
-#ifndef _MONTY_
-#define _MONTY_
-
-/* Constants */
-#define SUCSS_OP		0
-#define VALID_PARM		0
-#define MIN_ARGS		2
-#define METH_STACK		300
-#define METH_QUEUE		301
-
-/* Common Errors */
-#define ERR_BAD_INST	100
-#define ERR_BAD_MALL	101
-#define ERR_INVLD_PARM	102
-
-/* Usage Errors */
-#define ERR_ARG_USG		200
-#define ERR_PUSH_USG	201
-#define ERR_PINT_USG	202
-#define ERR_POP_USG		203
-#define ERR_SWAP_USG	204
-#define ERR_ADD_USG		205
-#define ERR_SUB_USG		206
-#define ERR_DIV_USG		207
-#define ERR_DIV_ZRO		208
-#define ERR_MUL_USG		209
-#define ERR_MOD_USG		210
-#define ERR_PCH_USG		211
-#define ERR_PCH_EMP		212
-
-#include <ctype.h>
-#include <fcntl.h>
-#include <stdlib.h>
+#ifndef MONTY_H
+#define MONTY_H
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
-#include <unistd.h>
 
+#define DELIMATOR "\n\t\r "
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -43,7 +14,7 @@
  *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO Holberton project
- */
+ **/
 typedef struct stack_s
 {
 	int n;
@@ -58,44 +29,30 @@ typedef struct stack_s
  *
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO Holberton project
- */
+ **/
 typedef struct instruction_s
 {
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-extern stack_t *head;
+/**
+* struct global_s - hold global variables
+* @return_val: return value
+* @mol: mode of list
+* @cc: code command
+* @apc: push command
+**/
 
-void check_args_num(int argn);
-FILE *open_file(char *filename);
-void check_access_rights(char *filename);
-int check_push_param(char *param);
-int check_digits(char *s);
-void frees_stack(void);
-int handle_execution(char *op_code, char *op_param, unsigned int line, int m);
-void handle_error(int errno, char *opcode, unsigned int line, char *buff);
-void handle_cerror(int errno, char *opcode, unsigned int line);
-void handle_uerror(int errno, unsigned int line);
-void handle_more_uerror(int errno, unsigned int line);
-void (*pick_func(char *s))(stack_t **, unsigned int);
-unsigned int count_stack(stack_t *stack);
-void push(stack_t **stack, unsigned int param);
-void push_queue(stack_t **stack, unsigned int param);
+extern instruction_t opcodes[];
+unsigned int len(stack_t **stack);
+void free_stack(stack_t *stack);
+void nop(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, char *n, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
+int find_opcode(stack_t **stack, char *opcode, int line_number);
 void swap(stack_t **stack, unsigned int line_number);
-void add(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
-void sub(stack_t **stack, unsigned int line_number);
-void divide(stack_t **stack, unsigned int line_number);
-void mul(stack_t **stack, unsigned int line_number);
-void mod(stack_t **stack, unsigned int line_number);
-void pchar(stack_t **stack, unsigned int line_number);
-void pstr(stack_t **stack, unsigned int line_number);
-void rotl(stack_t **stack, unsigned int line_number);
-void rotr(stack_t **stack, unsigned int line_number);
-int getline(char **buffer, size_t *line_number, FILE *frd);
-
+size_t lol(stack_t *h);
 #endif
